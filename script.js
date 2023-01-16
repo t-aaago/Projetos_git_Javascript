89657437b12f1bea60cdfc514dccd5448ceb0c48
t_aaago <tiagolivieira003@gmail.com>
2023-01-16 14:05:59 -0300

    função dentro de objetos

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const nome_completo = (sob) => 'Tiago ' + sob
+/* primeira versão com função anonima
+let pessoa = {
+    nome: 'Tiago',
+    sobrenome: 'Oliveira',
+    idade: 90,
+    nomeCompleto : () => {
+        completo = `${nome} ${sobrenome}`
+        return completo
+    }
+}*/
 
-console.log(nome_completo('Oliveira'))
+
+//----------------------------------------------------
+
+/* segunda versão com 'this'
+console.log(pessoa.nomeCompleto(pessoa.nome, pessoa.sobrenome))
+
+let pessoa = {
+    nome: 'Tiago',
+    sobrenome: 'Oliveira',
+    idade: 90,
+    nomeCompleto: function(){
+        return `${this.nome} ${this.sobrenome}`
+    }
+}
+
+//"this" refere-se ao próprio objeto
+
+console.log(pessoa.nomeCompleto())
+*/
