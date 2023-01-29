0daadcf0a72cf6f82aacdf26b61920c197e97ba9
Tiago <105828531+t-aaago@users.noreply.github.com>
2023-01-29 08:23:38 -0300

    Update console.js

--- console.js
+++ console.js
@@ -1,4 +1,5 @@
-console.log(process.argv)
+//get recebe o modulo criado no arquivo getflag.js
 get = require('/home/tiago/Javascript/NodeJS/getflag.js')
 
+//apenas um teste do uso de flags
 console.log(`Olá ${get('--nome')} ${get('--sobrenome')}, tudo bom?`)
